cd9ca55171d2678cfee64cb3be4b33b422566de2
Alex <al.repin.a@yandex.ru>
2022-11-24 12:56:31 +0300

    add controller

---
+++ controller.py
@@ -0,0 +1,7 @@
+from smash import sort, get_da
+from viev import number_operation, abon_data
+from read_write_file import read_file, write_file
+from test_keys import test_data
+
+def start():
+    return test_data(number_operation, read_file, write_file, sort, get_da, abon_data)
